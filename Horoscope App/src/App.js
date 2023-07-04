4da3b862e641ac03e0cc98d766dd5082f36cf874
Ali <alidrl26@gmail.com>
2023-07-04 19:03:51 +0200

    restarting project

--- Horoscope App/src/App.js
+++ Horoscope App/src/App.js
@@ -1,18 +1,9 @@
-import Header from "./components/header/Header";
-import Main from "./components/main/Main";
-import Navbar from "./components/navbar/Navbar";
-
-import "./sass/_reset.scss"
-
-import "./app.scss"
 function App() {
   return (
-    <>
-      <Navbar />
-      <Header />
-      <Main />
-    </>
-  );
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
