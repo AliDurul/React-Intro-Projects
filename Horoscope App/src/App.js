baecaaaed2bbb52dcc754843e0d8b0528712ae80
Ali <alidrl26@gmail.com>
2023-07-04 22:13:26 +0200

    project completed

--- Horoscope App/src/App.js
+++ Horoscope App/src/App.js
@@ -1,9 +1,14 @@
 import "./App.scss";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Learn React</header>
+      <Navbar />
+      <Header />
+      <Main />
     </div>
   );
 }
