60867363bb72c8c26649053bde4c6b8765a68b6f
Ali <alidrl26@gmail.com>
2023-07-04 14:41:38 +0200

    initial commit

---
+++ Horoscope App/src/App.js
@@ -0,0 +1,22 @@
+
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Navbar from "./components/navbar/Navbar";
+import  data from "./helper/data"
+
+function App() {
+  return (<>
+
+   <Navbar/>
+   <Header/>
+   <Main/>
+
+
+  </>
+ 
+  )
+  
+  
+}
+
+export default App;
---
+++ Horoscope App/src/components/main/Main.js
@@ -0,0 +1,12 @@
+import Card from "./Card"
+
+
+const Main = () => {
+  return (
+    <div>Main
+      <Card/>
+    </div>
+  )
+}
+
+export default Main
