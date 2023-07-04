e215c2cf71dc9d36dd2873bdc1a7ae258197b8e9
Ali <alidrl26@gmail.com>
2023-07-04 15:52:47 +0200

    navbar edited

--- Horoscope App/src/App.js
+++ Horoscope App/src/App.js
@@ -1,22 +1,18 @@
-
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Navbar from "./components/navbar/Navbar";
-import  data from "./helper/data"
+import data from "./helper/data";
+
+import "./sass/_reset.scss"
 
 function App() {
-  return (<>
-
-   <Navbar/>
-   <Header/>
-   <Main/>
-
-
-  </>
- 
-  )
-  
-  
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
--- Horoscope App/src/components/navbar/Navbar.js
+++ Horoscope App/src/components/navbar/Navbar.js
@@ -1,9 +1,29 @@
-
+import logo from "../../helper/logo/logo.png"
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="Navbar">
+      <img src={logo} alt="" />
+      <ul>
+        <li>
+          <a href="#">HOROSCOPE</a>
+        </li>
+        <li>
+          <a href="#">DAILY</a>
+        </li>
+        <li>
+          <a href="#">TAROT</a>
+        </li>
+        <li>
+          <a href="#">ARTICLE</a>
+        </li>
+        <li>
+          <a href="#">CONTACT</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
