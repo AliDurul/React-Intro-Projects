4870a3eca609e0a127d9af4d23785e2907353c5d
Ali <alidrl26@gmail.com>
2023-07-07 09:33:44 +0200

    multiple search completed

--- SearchBar/src/compenents/Main.jsx
+++ SearchBar/src/compenents/Main.jsx
@@ -1,34 +1,42 @@
 
 import { useState } from "react"
 import { Users } from "../helper/users"
+import Table from "./Table"
 
 const Main = () => {
 
-    const [query, setQuery] = useState("")
+    const [query, setQuery] = useState("");
+    // console.log(query);
 
-    // console.log(Users);
+    // console.log(Users[0]["email"]);
 
-    let nameArr = []
-    nameArr = Users.filter((user) => user.first_name.toLowerCase().includes(query))
-console.log(nameArr);
-
-
-
-    const handleSearch = (e) => {
-        // setQuery(e.target.value)
-        setQuery(e.target.value)
-console.log(query);
+    /* search button long way */
+    // const search = (data) => {
+    //     return data.filter(item => item.first_name.toLowerCase().includes(query) || item.last_name.toLowerCase().includes(query) || item.email.toLowerCase().includes(query))
+    // }
+    
+    const keys = ['first_name', 'last_name', 'email'];
+    const search = (data) => {
+        return data.filter(item => keys.some(key => item[key].toLowerCase().includes(query)))
     }
 
-    return (
-        <div>
-            <input type="text" className="search" onChange={handleSearch} />
 
-            <ul className="list">
-                {
-                    Users.map((user) => <li className="listItem">{user.first_name}</li>)
-                }
-            </ul>
+
+
+    // console.log(search(Users));
+    return (
+        <div className="app">
+            <input type="text" className="search" onChange={(e) => setQuery(e.target.value)} />
+            {/* <ul className="list">
+            {
+                Users.filter(user => user.first_name.toLowerCase().includes(query)).map(user => (
+                <li className="listItem">{user.first_name}</li>
+
+                ) )
+            }
+            </ul> */}
+
+            <Table data={search(Users)} />
 
         </div>
     )
