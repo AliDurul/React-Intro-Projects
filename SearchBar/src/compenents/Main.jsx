c09dffbfb13f4d9fe449b17c2c9a59549a2a2a63
Ali <alidrl26@gmail.com>
2023-07-07 18:51:24 +0200

    bug solved

--- SearchBar/src/compenents/Main.jsx
+++ SearchBar/src/compenents/Main.jsx
@@ -6,40 +6,17 @@ import Table from "./Table"
 const Main = () => {
 
     const [query, setQuery] = useState("");
-    // console.log(query);
-
-    // console.log(Users[0]["email"]);
-
-    /* search button long way */
-    // const search = (data) => {
-    //     return data.filter(item => item.first_name.toLowerCase().includes(query) || item.last_name.toLowerCase().includes(query) || item.email.toLowerCase().includes(query))
-    // }
-    
+  
     const keys = ['first_name', 'last_name', 'email'];
+    
     const search = (data) => {
         return data.filter(item => keys.some(key => item[key].toLowerCase().includes(query)))
     }
-
-
-
-
-    // console.log(search(Users));
     return (
         <div className="app">
             <input type="text" className="search" onChange={(e) => setQuery(e.target.value)} />
-            {/* <ul className="list">
-            {
-                Users.filter(user => user.first_name.toLowerCase().includes(query)).map(user => (
-                <li className="listItem">{user.first_name}</li>
-
-                ) )
-            }
-            </ul> */}
-
-            <Table data={search(Users)} />
-
+            <Table  data = {search(Users)} />
         </div>
     )
 }
-
 export default Main
--- SearchBar/src/compenents/Table.jsx
+++ SearchBar/src/compenents/Table.jsx
@@ -1,33 +1,33 @@
 
 
-const Table = ( {data} ) => {
+const Table = ({ data }) => {
     // let dataArr = props.data
     // const [id] = dataArr\
-    console.log(data);
+    // console.log(data);
     return (
-        <div>
-            <table>
-                <tr>
-                    <th>Id</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                </tr>
-                {
-                    data.map((item) =>
-                    (
-                        <tr>
-                            <td>{item.id}</td>
-                            <td>{item.first_name}</td>
-                            <td>{item.last_name}</td>
-                            <td>{item.email}</td>
-                        </tr>
-                    )
-                    )
-                }
 
-            </table>
-        </div>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+            </tr>
+            {
+                data.map((item) =>
+                (
+                    <tr>
+                        <td>{item.id}</td>
+                        <td>{item.first_name}</td>
+                        <td>{item.last_name}</td>
+                        <td>{item.email}</td>
+                    </tr>
+                )
+                )
+            }
+
+        </table>
+
     )
 }
 
