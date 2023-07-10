9fba18f32b59cbd5cc454d5e730a6b5c99a5f947
Ali <alidrl26@gmail.com>
2023-07-10 07:32:39 +0200

    updating form and events

--- Basic-Forms-Example/src/App.js
+++ Basic-Forms-Example/src/App.js
@@ -1,40 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 export default function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    address: "",
+    email: "",
+  });
+
+  const { firstName, lastName, phoneNumber, address, email } = values;
+
+  const handleValues = (e) => {
+    setValues({...values, [e.target.name]:e.target.value})
+  };
+console.log(firstName);
   return (
-    <div class="form-container">
-      <form class="register-form">
+    <div className="form-container">
+      <form className="register-form">
         {/* Uncomment the next line to show the success message */}
         {/* <div class="success-message">Success! Thank you for registering</div> */}
         <input
+          onChange={handleValues}
+          value={firstName}
           id="first-name"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="First Name"
           name="firstName"
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="first-name-error">Please enter a first name</span> */}
         <input
           id="last-name"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Last Name"
           name="lastName"
+        />
+        <input
+          id="phone-number"
+          className="form-field"
+          type="text"
+          placeholder="Phone Number"
+          name="phoneNumber"
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="first-name-error">Please enter a first name</span> */}
+        <input
+          id="adress"
+          className="form-field"
+          type="text"
+          placeholder="Adress"
+          name="address"
         />
         {/* Uncomment the next line to show the error message */}
         {/* <span id="last-name-error">Please enter a last name</span> */}
         <input
           id="email"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Email"
           name="email"
         />
         {/* Uncomment the next line to show the error message */}
         {/* <span id="email-error">Please enter an email address</span> */}
-        <button class="form-field" type="submit">
+        <button className="form-field" type="submit">
           Register
         </button>
       </form>
