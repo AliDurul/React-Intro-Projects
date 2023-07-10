d669446cdb9a186aca9be4d59314e86de3244aa1
Ali <alidrl26@gmail.com>
2023-07-10 15:49:31 +0200

    updated with warning the user

--- Basic-Forms/src/App.js
+++ Basic-Forms/src/App.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 
 export default function App() {
   const [values, setValues] = useState({
     firstName: "",
     lastName: "",
-    phoneNumber: "",
-    address: "",
-    email: "",
+    PhoneNumber: "",
+    adress: "",
+    email: ""
   });
 
-  const [submitted, setSubmitted] = useState(false);
-  const [isvalid, setIsvalid] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const { firstName, lastName, phoneNumber, address, email } = values;
+  const { firstName, lastName, PhoneNumber, adress, email } = values;
 
   const handleValues = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -21,72 +20,92 @@ export default function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (firstName && lastName && phoneNumber && address && email){
-      setIsvalid(true)
-    }
-      setSubmitted(true);
+    setIsSubmitted(true);
+    setValues({
+      firstName: "",
+      lastName: "",
+      PhoneNumber: "",
+      adress: "",
+      email: ""
+    })
   };
 
-  console.log(submitted);
+  useEffect(() => {
+    let timeout;
+
+    if (isSubmitted) {
+      timeout = setTimeout(() => {
+        setIsSubmitted(false);
+      }, 5000); // 5 seconds
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [isSubmitted]);
+
   return (
     <div className="form-container">
       <form className="register-form" onSubmit={handleSubmit}>
-        {submitted && isvalid && (
+        {isSubmitted && (
           <div className="success-message">
             Success! Thank you for registering
           </div>
         )}
+
         <input
           onChange={handleValues}
-          value={firstName}
           id="first-name"
           className="form-field"
           type="text"
           placeholder="First Name"
           name="firstName"
+          required
+          value={firstName}
         />
-        {submitted && !firstName && (
-          <span id="first-name-error">Please enter a first name</span>
-        )}
-
         <input
           onChange={handleValues}
-          value={lastName}
           id="last-name"
           className="form-field"
           type="text"
           placeholder="Last Name"
           name="lastName"
+          required
+          value={lastName}
         />
         <input
           onChange={handleValues}
-          value={phoneNumber}
           id="phone-number"
           className="form-field"
           type="text"
           placeholder="Phone Number"
-          name="phoneNumber"
+          name="PhoneNumber"
+          required
+          value={PhoneNumber}
         />
         {/* <span id="first-name-error">Please enter a first name</span> */}
         <input
           onChange={handleValues}
-          value={address}
           id="adress"
           className="form-field"
           type="text"
           placeholder="Adress"
-          name="address"
+          name="adress"
+          required
+          value={adress}
         />
+     
         {/* Uncomment the next line to show the error message */}
         {/* <span id="last-name-error">Please enter a last name</span> */}
         <input
           onChange={handleValues}
-          value={email}
           id="email"
           className="form-field"
-          type="text"
+          type="email"
           placeholder="Email"
           name="email"
+          required
+          value={email}
         />
         {/* Uncomment the next line to show the error message */}
         {/* <span id="email-error">Please enter an email address</span> */}
