04b2ccddcdaad94dedd9db08c0feb5a799e1d055
Ali <alidrl26@gmail.com>
2023-07-14 18:38:17 +0200

    Card - Hedaer components created

--- Random User App/src/App.js
+++ Random User App/src/App.js
@@ -1,93 +1,50 @@
 import React, { useEffect, useState } from "react";
-import mailSvg from "./assets/mail.svg";
-import manSvg from "./assets/man.svg";
-import womanSvg from "./assets/woman.svg";
-import manAgeSvg from "./assets/growing-up-man.svg";
-import womanAgeSvg from "./assets/growing-up-woman.svg";
-import mapSvg from "./assets/map.svg";
-import phoneSvg from "./assets/phone.svg";
-import padlockSvg from "./assets/padlock.svg";
-import cwSvg from "./assets/cw.svg";
-import Footer from "./components/footer/Footer";
+
+/* import Footer from "./components/footer/Footer"; */
+
+import axios from "axios";
+import Header from "./components/Header";
+import Card from "./components/Card";
 
 const url = "https://randomuser.me/api/";
-const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
-const [randomPerson, setRandomPerson] = useState([])
-
-
-  const getData = async () => {
+  const [randomPerson, setRandomPerson] = useState([]);
+  /* ASYNC FUNCTION WITH FETCH */
+  /*  const getData = async () => {
     const response = await fetch(url);
-    const data = await response.json()
-    return data.results[0]
-  }
+    const data = await response.json();
+    return data.results[0];
+  };
 
-  
   useEffect(() => {
-    
-    getData().then(data => setRandomPerson(data) )
-  
- }, [])
- 
- console.log(randomPerson);
+    getData()
+      .then((data) => setRandomPerson(data))
+      .catch((err) => console.log(err.mese));
+  }, []); */
+
+  /* // AXIOS 
+   axios.get(url).then((res) => console.log(res.data.results[0])); */
+
+  /* ASYNC FUNCTION WITH AXIOS */
+  const fetchData = async () => {
+    const request = await axios.get(url);
+    setRandomPerson([request.data.results[0]]);
+    return request;
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(randomPerson);
   return (
     <main>
-      <div className="block bcg-orange">
-        <img src={cwSvg} alt="cw" id="cw" />
-      </div>
-      <div className="block">
-        <div className="container">
-          <img src={defaultImage} alt="random user" className="user-img" />
-          <p className="user-title">My ... is</p>
-          <p className="user-value"></p>
-          <div className="values-list">
-            <button className="icon" data-label="name">
-              <img src={womanSvg} alt="user" id="iconImg" />
-            </button>
-            <button className="icon" data-label="email">
-              <img src={mailSvg} alt="mail" id="iconImg" />
-            </button>
-            <button className="icon" data-label="age">
-              <img src={womanAgeSvg} alt="age" id="iconImg" />
-            </button>
-            <button className="icon" data-label="street">
-              <img src={mapSvg} alt="map" id="iconImg" />
-            </button>
-            <button className="icon" data-label="phone">
-              <img src={phoneSvg} alt="phone" id="iconImg" />
-            </button>
-            <button className="icon" data-label="password">
-              <img src={padlockSvg} alt="lock" id="iconImg" />
-            </button>
-          </div>
-          <div className="btn-group">
-            <button className="btn" type="button">
-              new user
-            </button>
-            <button className="btn" type="button">
-              add user
-            </button>
-          </div>
-
-          <table className="table">
-            <thead>
-              <tr className="head-tr">
-                <th className="th">Firstname</th>
-                <th className="th">Email</th>
-                <th className="th">Phone</th>
-                <th className="th">Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="body-tr"></tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <Header />
+      <Card />
+      {/* <div style={{ display: "flex", justifyContent: "center" }}>
         <Footer />
-      </div>
+      </div> */}
     </main>
   );
 }
