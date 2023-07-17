d88c0d339cd946dec72d89d2b5f4503eba02293c
Ali <alidrl26@gmail.com>
2023-07-17 20:24:58 +0200

    Home: data fetched from database

--- Tutorial App/src/pages/Home.jsx
+++ Tutorial App/src/pages/Home.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
-
+import axios from "axios"
 const Home = () => {
+
+  const [tutorials, setTutorials] = useState()
+
+  const base_url = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+
+  const getTutorials = async () => {
+    try {
+      const {data} = await axios.get(base_url)
+      setTutorials(data)
+      console.log(tutorials);
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  useEffect(() => {
+    getTutorials()
+
+  }, [])
+
+
   return (
     <>
       <AddTutorial />
-      <TutorialList />
+      <TutorialList tutorials={tutorials} />
     </>
   );
 };
