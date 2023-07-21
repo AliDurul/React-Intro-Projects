d45bbeb4aed81b7fe0b0f1bf38edff7b2e72be2e
Ali <alidrl26@gmail.com>
2023-07-21 14:51:47 +0200

    css part has completed

--- ProductList/src/components/ProductCard.jsx
+++ ProductList/src/components/ProductCard.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 
 const ProductCard = ({ data }) => {
   return (
-    <div className='d-flex flex-wrap gap-3 mt-5 justify-content-center'>
+    <div className='d-flex flex-wrap gap-5  mt-5 justify-content-center'>
 
       {
         data?.map((item) => {
           const { id, image, price, title } = item
           return (
-            <div key={id} className="card" style={{ width: '18rem' }}>
-              <div className='img-card'>
+            <div key={id} className="card p-2 " style={{ width: '18rem' }}>
+              <div className='img-card border border-gray'>
                 <img src={image} className="card-img-top" alt="..." />
               </div>
-              <div className="card-body">
-                <h5 className="card-title">{title}</h5>
+              <div className="card-price">
                 <p className="card-text">{price}</p>
               </div>
+              <div className="card-cover">
+                <h5 className="card-title">{title}</h5>
+              </div>
+
             </div>
           )
         })
